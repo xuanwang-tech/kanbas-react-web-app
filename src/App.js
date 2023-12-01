ebafb02ed95e3964dadd3d87c01ccfa0ce932d0e
xuanwang <wang.xuan5@northeastern.edu>
2023-12-01 16:36:12 -0600

    a6

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import {HashRouter} from "react-router-dom";
 import {Routes, Route, Navigate} from "react-router";
-
+import Project from "./Project";
 
 function App() {
 
@@ -15,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/"         element={<Navigate to="/Labs"/>}/>
           <Route path="/hello"    element={<HelloWorld/>}/>
+          <Route path="/project/*" element={<Project />} />
           <Route path="/Labs/*"   element={<Labs/>}/>
           <Route path="/Kanbas/*" element={<Kanbas/>}/>
         </Routes>
