01cbb2fb3baa314a43dcd5d72e66ef6afd09f17b
xuanwang <wang.xuan5@northeastern.edu>
2023-12-05 15:02:18 -0600

    a

--- src/Project/users/client.js
+++ src/Project/users/client.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const request = axios.create({
-  withCredentials: true,
-});
+
 
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
-
+const request = axios.create({
+  withCredentials: true,
+});
 export const signin = async (credentials) => {
   const response = await request.post(`${USERS_API}/signin`, credentials);
   return response.data;
