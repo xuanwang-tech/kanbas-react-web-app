e4dfdaf36371f2d5c1bcac4a87787787cffcf5e8
xuanwang <wang.xuan5@northeastern.edu>
2023-12-05 21:56:43 -0600

    ""

--- src/Project/users/account.js
+++ src/Project/users/account.js
@@ -163,7 +163,8 @@ function Account() {
     }, []);
     return (
         <div className="w-50">
-            <h1>Account-111</h1>
+            <h1>Account</h1>
+            <h2>Status: {account ? 'Account is loaded' : 'Account is null'}</h2>
             {account && (
                 <div>
                    <h1>1111111</h1>
